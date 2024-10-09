1655b41fe7d30034b5621ed2063f912a3a1e64f1
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-09 14:11:12 -0400

    created Restaurant model and Review model that inherits from Restaurant model, aslo started to create base and home html pages

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    """Restaurant model"""
+
+    name = models.CharField(max_length="150")
+
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+
+class Review(models.Model):
+    """Model for reviews"""
+
+    restaurant_id = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="reviews")
+
+    user_id = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="reviews")
+
+    body = models.TextField()
+
+    rating = models.IntegerChoices(1, 5)
+
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
