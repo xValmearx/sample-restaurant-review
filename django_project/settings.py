40ca73bc8912275c89229fbc32ea3e14529485c3
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-09 13:33:16 -0400

    renamed and deleted app and data base again, I renamed it restaurants, since the app is about restaurants.

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "reviews.apps.ReviewsConfig",
 ]
 
 ROOT_URLCONF = "django_project.urls"
