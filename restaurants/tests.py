e7ecd47fe6e6771905768e680e283f17153ed450
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-24 15:59:27 -0400

    created test for each page details and template used

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -35,7 +35,7 @@ class ReviewTest(TestCase):
         self.assertEqual(self.review.user.username, "testuser")
         self.assertEqual(self.review.body, "i like ihop")
         self.assertEqual(self.review.rating, "5")
-        self.assertEqual(self.review.get_absolute_url(), "/review/1")
+        self.assertEqual(self.review.get_absolute_url(), "/review/1/")
 
     def test_url_exist_at_correct_location_restaurant_listview(self):
         """Test URL exist at correct location"""
@@ -46,16 +46,66 @@ class ReviewTest(TestCase):
     def test_url_exist_at_correct_location_restaurant_detailview(self):
         """test URL exist at correct location for the detailview"""
 
-        response = self.client.get("/restaurant/1")
-
+        response = self.client.get("/restaurant/1/")
         self.assertEqual(response.status_code, 200)
 
-    def test_post_listview(self):
+    def test_url_exist_at_correct_location_review_detailview(self):
+        """test URL exist at correct location for the detailview"""
 
-        response = self.client.get("/restaurant/1")
-
+        response = self.client.get("/review/1/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_exist_at_correct_location_restaurant_createview(self):
+        """test URL exist at correct location for the detailview"""
+
+        response = self.client.get("/review/new/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exist_at_correct_location_review_deleteview(self):
+        """test URL exist at correct location for the detailview"""
+
+        response = self.client.get("/review/1/delete/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exist_at_correct_location_review_update(self):
+        """test URL exist at correct location for the detailview"""
+
+        response = self.client.get("/review/1/edit/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_restsurant_detailview(self):
+        """test restaurant detail page"""
+        response = self.client.get("/restaurant/1/")
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, "i like ihop")
-
         self.assertTemplateUsed(response, "restaurant_details.html")
+
+    def test_review_detailview(self):
+        """Test review detail page"""
+        response = self.client.get("/review/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "ihop")
+        self.assertContains(response, "testuser")
+        self.assertContains(response, "i like ihop")
+        self.assertTemplateUsed(response, "review_details.html")
+
+    def test_review_deleteview(self):
+        """test review delete page"""
+        response = self.client.get("/review/1/delete/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Are you sure you want to delete")
+        self.assertTemplateUsed(response, "review_delete.html")
+
+    def test_review_newview(self):
+        """test review new page"""
+        response = self.client.get("/review/new/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "create new review")
+        self.assertTemplateUsed(response, "review_new.html")
+
+    def test_review_updateview(self):
+        """test review update page"""
+        response = self.client.get("/review/1/edit/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "are you sure you want to edit this review")
+        self.assertTemplateUsed(response, "review_update.html")
--- restaurants/urls.py
+++ restaurants/urls.py
@@ -7,8 +7,8 @@ from .views import RestaurantView, RestaurantDetailView, CreateReview, DetailedR
 
 urlpatterns = [
     path("review/new/", CreateReview.as_view(), name="review_new"),
-    path("review/<int:pk>", DetailedReview.as_view(), name="review_details"),
-    path("restaurant/<int:pk>", RestaurantDetailView.as_view(), name="restaurant_details"),
+    path("review/<int:pk>/", DetailedReview.as_view(), name="review_details"),
+    path("restaurant/<int:pk>/", RestaurantDetailView.as_view(), name="restaurant_details"),
     path("review/<int:pk>/edit/", UpdateReview.as_view(), name="review_update"),
     path("review/<int:pk>/delete/", DeleteReview.as_view(), name="review_delete"),
     path("", RestaurantView.as_view(), name="home"),
