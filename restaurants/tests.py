25412e3af85d4414cb5cfe9d81497da993d23b59
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-24 12:00:16 -0400

    creates tesing for the user modle

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -2,41 +2,37 @@
 # CIS 218
 # 10/24/2024
 
-from django.contrib.auth import get_user_model
-
 from django.test import TestCase
+
+from django.contrib.auth import get_user_model
 
 from django.urls import reverse
 
+from .models import Restaurant, Review
 
 # Create your tests here.
 
 
-class SignUpPageTest(TestCase):
-    """Sign Up Page Test"""
+class ReviewTest(TestCase):
+    """Test for the review the"""
 
-    def test_url_exist_at_correct_signupview(self):
-        """Test url exist at correct location in the sign up view"""
-        response = self.client.get("/accounts/signup/")
-        self.assertEqual(response.status_code, 200)
+    @classmethod
+    def setUpTestData(cls):
+        """setting up test user"""
+        cls.user = get_user_model().objects.create_user(username="testuser", email="test@email.com", password="secret")
 
-    def test_signup_view_name(self):
-        """Test signup view name"""
-        response = self.client.get(reverse("signup"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "registration/signup.html")
+        cls.restaurant = Restaurant.objects.create(name="ihop")
 
+        cls.review = Review.objects.create(
+            restaurant=cls.restaurant,
+            user=cls.user,
+            body="i like ihop",
+            rating="5",
+        )
 
-class LogInPageTest(TestCase):
-    """LogIn Page Test"""
-
-    def test_url_exist_at_correct_signupview(self):
-        """Test url exist at correct location in the Log In view"""
-        response = self.client.get("/accounts/login/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_signup_view_name(self):
-        """Test login view name"""
-        response = self.client.get(reverse("login"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "registration/login.html")
+    def test_user_model(self):
+        self.assertEqual(self.review.restaurant.name, "ihop")
+        self.assertEqual(self.review.user.username, "testuser")
+        self.assertEqual(self.review.body, "i like ihop")
+        self.assertEqual(self.review.rating, "5")
+        self.assertEqual(self.review.get_absolute_url(), "/review/1")
