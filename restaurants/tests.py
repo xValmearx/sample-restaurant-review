1b35ad2f4fbcfcafaeb898e01038dc2cf2b25ae3
Caleb Taylor <calebs.phone2017@gmail.com>
2024-10-24 11:38:47 -0400

    created testing for login page

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -25,3 +25,18 @@ class SignUpPageTest(TestCase):
         response = self.client.get(reverse("signup"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "registration/signup.html")
+
+
+class LogInPageTest(TestCase):
+    """LogIn Page Test"""
+
+    def test_url_exist_at_correct_signupview(self):
+        """Test url exist at correct location in the Log In view"""
+        response = self.client.get("/accounts/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        """Test login view name"""
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/login.html")
